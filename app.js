3dc273663227fe7b31cc52471398686449f99a80
DiegoPJ <guit0o@hotmail.com>
2020-01-25 18:22:34 +0100

    terer commmiiit

--- app.js
+++ app.js
@@ -6,34 +6,43 @@ window.addEventListener("load" , () => {
     let temperatureDescription = document.querySelector(".temperature-description");
     let temperatureDegree = document.querySelector(".temperature-degree");
     let timeZone = document.querySelector(".location-timezone");
-    let iconoHtml = document.querySelector("icon1");
 
     if(navigator.geolocation){
+       
+        
         navigator.geolocation.getCurrentPosition( (position) => {
             long = position.coords.longitude;
             lat = position.coords.latitude;
             console.log(`Latitud: ${lat} - Longitud: ${long}`);
-
             let proxy = "https://cors-anywhere.herokuapp.com/";
             let url = `${proxy}https://api.darksky.net/forecast/f8767ca1a9c4baca8a580072e3831b54/${lat},${long}`;
             fetch(url)
-            .then(Response => {return Response.json()})
+            .then(response => {return response.json()})
             .then(datos => { 
                 const {temperature,summary, icon} = datos.currently;
                 temperatureDescription.textContent = summary;
                 temperatureDegree.textContent = temperature;
                 timeZone.textContent = datos.timezone;
-                setIcon(icon,iconoHtml);
-            });
-        })
-        function setIcon(icon, iconID) {
-            let skycons = new Skycons({"color": "pink"});
-            const currentIcon = iconId.replace(/-/g, "_").toUpperCase();
-            skycons.play;
-            return skycons.set("iconID", Skycons[currentIcon]);
+                setIcons(icon, document.querySelector(".icon"));
 
-
-        }
+                    temperatureSection.addEventListener("click", () => {
+                        if (temperatureSpan.textContent === "F") {
+                            temperatureSpan.textContent = "C";
+                            temperatureDegree.textContent = Math.floor(celsius);
+                        } else {
+                            temperatureSpan.textContent = "F";
+                            temperatureDegree.textContent = temperature;
+                        }
+                    });
+                });
+        });
+   
+function setIcons(icon, iconID) {
+        const skycons = new Skycons({ color: "white" });
+        const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+        skycons.play();
+        return skycons.set(iconID, Skycons[currentIcon]);
+    }
     }else{
         alert("Primoo, activa la geolocalizacion!!!!!!!");
     }
